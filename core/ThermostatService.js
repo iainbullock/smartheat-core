924a08836dc3eb0339fd8d2be4769fb37754a685
iainbullock <59146297+iainbullock@users.noreply.github.com>
2020-02-02 16:51:16 +0000

    Update ThermostatService.js

--- core/ThermostatService.js
+++ core/ThermostatService.js
@@ -113,7 +113,7 @@ class ThermostatService extends Service {
             const thermostat = await this.obtainThermostat();
 
             if (onOff === 'on' && (device.currentTemperature > targetTemperature) ) {
-                targetTemperature = parseFloat(device.currentTemperature.toFixed(0)) + 0.5;
+                targetTemperature = parseFloat(device.currentTemperature).floor() + 1;
                 this._logger.debug(`Updating requested temperature to ${targetTemperature}...`);
             }
 messages = messages.concat(`New target ${targetTemperature}. Device temp ${device.currentTemperature}`)
