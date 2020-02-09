21b1f397eca28414dda6fe520a931cf7498b6f9c
iainbullock <59146297+iainbullock@users.noreply.github.com>
2020-02-09 09:41:38 +0000

    Update ThermostatService.js

--- core/ThermostatService.js
+++ core/ThermostatService.js
@@ -65,7 +65,7 @@ class ThermostatService extends Service {
 
             let formatted_device_time = Date.parse(device.time.toLocaleString());
 /*            let timeDelta = Date() - Date.parse(formatted_device_time);*/
-            let timeDelta = Date.parse(formatted_device_time);
+            let timeDelta = Date();
 
             const messages = [];
             messages.push(`The device time is ${formatted_device_time}, which is ${timeDelta} seconds fast`);
